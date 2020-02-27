72d830485f0ebfd7afbbe826850d0c211c59cf3c
Aditi Mittal <mittaladiti15@gmail.com>
2020-02-27 23:28:16 +0000

    model updated

--- articleStore/models/modelArticle.go
+++ articleStore/models/modelArticle.go
@@ -4,8 +4,8 @@ package models
 type Article struct {
 	Body string `json:"body,omitempty"`
 	Date string `json:"date"`
-	ID   string `json:"id"`
+	ID   string `json:"id,omitempty"`
 	//minItems=1,
 	Tags  []string `json:"tags"`
-	Title string   `json:"title"`
+	Title string   `json:"title,omitempty"`
 }
